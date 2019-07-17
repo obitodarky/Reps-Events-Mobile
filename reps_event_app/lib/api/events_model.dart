aac225d03a041e6492547d0284a457456ba0fb90
Harsh Mehta <deadspaceharsh@gmail.com>
2019-07-17 16:43:47 +0530

    added events model

---
+++ reps_event_app/lib/api/events_model.dart
@@ -0,0 +1,12 @@
+class EventsModel{
+  final String name;
+  final String city; 
+  final String country;
+  final String description;
+  final String end;
+  final String eventUrl;
+  final String start;
+  final String venue;
+  
+  EventsModel(this.name, this.city, this.country, this.description, this.end, this.eventUrl, this.start, this.venue);
+}
