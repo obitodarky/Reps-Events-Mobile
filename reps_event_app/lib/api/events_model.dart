ce1cd60e95cef96cc0a0a9186cf0f0d8558e0a6f
Harsh Mehta <deadspaceharsh@gmail.com>
2019-07-18 22:31:40 +0530

    added factory method

--- reps_event_app/lib/api/events_model.dart
+++ reps_event_app/lib/api/events_model.dart
@@ -4,9 +4,34 @@ class EventsModel{
   final String country;
   final String description;
   final String end;
-  final String eventUrl;
+  final String event_url;
   final String start;
   final String venue;
   
-  EventsModel(this.name, this.city, this.country, this.description, this.end, this.eventUrl, this.start, this.venue);
+  EventsModel({
+    this.name, 
+    this.city,
+    this.country, 
+    this.description, 
+    this.end, 
+    this.event_url, 
+    this.start, 
+    this.venue
+  });
+
+  factory EventsModel.fromJson(Map<String, dynamic> parsedJson ){
+    return EventsModel(
+      name: parsedJson['name'],
+      city: parsedJson['city'],
+      country: parsedJson['country'],
+      description: parsedJson['description'],
+      end: parsedJson['end'],
+      event_url: parsedJson['event_url'],
+      start: parsedJson['start'],
+      venue: parsedJson['venue']
+
+    );
+  }
+
 }
+
