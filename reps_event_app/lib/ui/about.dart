6fc93953b51974acc4bf102f90ccb46135675c7e
Ankesh <ankesh.anku30@gmail.com>
2019-10-17 01:12:53 +0530

    Fix overflow error in about tab

--- reps_event_app/lib/ui/about.dart
+++ reps_event_app/lib/ui/about.dart
@@ -15,20 +15,24 @@ class _AboutState extends State<About> {
   Widget build(BuildContext context) {
     appBarSize = MediaQuery.of(context).size.height / 3.5;
     return Scaffold(
-      body: Column(
-        children: <Widget>[
-          CustomAppBar(appBarTitle: Utils.about_page, appBarSize: appBarSize,),
-          Padding(
-            padding: const EdgeInsets.only(top:20.0, bottom: 16.0, left: 16.0, right: 16.0),
-            child: Text(
-              Utils.about_info,
-              style: TextStyle(
-                fontSize: 28,
-                fontFamily: Utils.zilla_slab,
+      //SingleChildScrollView is used to handle cases when screen size is small
+      //enough to have all elements (Text) or fit completely.
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            CustomAppBar(appBarTitle: Utils.about_page, appBarSize: appBarSize,),
+            Padding(
+              padding: const EdgeInsets.only(top:20.0, bottom: 16.0, left: 16.0, right: 16.0),
+              child: Text(
+                Utils.about_info,
+                style: TextStyle(
+                  fontSize: 28,
+                  fontFamily: Utils.zilla_slab,
+                ),
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
